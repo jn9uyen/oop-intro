98973492e373a727de9bd7d1bda2e87e4f9663f9
Joe Nguyen <jnguyen.wrk@gmail.com>
2020-09-02 18:10:23 +1000

    refactor(): policy-based design

--- productivity.py
+++ productivity.py
@@ -1,7 +1,21 @@
 
 class ProductivitySystem:
+    def __init__(self):
+        self._roles = {
+            'manager': ManagerRole,
+            'secretary': SecretaryRole,
+            'sales': SalesRole,
+            'factory': FactoryRole,
+        }
+
+    def get_role(self, role_id):
+        role_type = self._roles.get(role_id)
+        if not role_type:
+            raise ValueError('role_id')
+        return role_type()
+
     def track(self, employees, hours):
-        print('\nTracking Employee Productivity')
+        print('Tracking Employee Productivity')
         print('==============================')
         for employee in employees:
             employee.work(hours)
@@ -9,24 +23,20 @@ class ProductivitySystem:
 
 
 class ManagerRole:
-    def work(self, hours):
-        print(f'screams and yells for {hours} hours.')
+    def perform_duties(self, hours):
+        return f'screams and yells for {hours} hours.'
 
 
 class SecretaryRole:
-    def work(self, hours):
-        print(f'expends {hours} hours doing office paperwork.')
+    def perform_duties(self, hours):
+        return f'does paperwork for {hours} hours.'
 
 
-class SalesPersonRole:
-    def work(self, hours):
-        print(f'expends {hours} hours on the phone.')
+class SalesRole:
+    def perform_duties(self, hours):
+        return f'expends {hours} hours on the phone.'
 
 
-class FactoryWorkerRole:
-    def work(self, hours):
-        print(f'manufactures gadgets for {hours} hours.')
-
-
-# class TemporarySecretary(Secretary, HourlyEmployee):
-#     pass
+class FactoryRole:
+    def perform_duties(self, hours):
+        return f'manufactures gadgets for {hours} hours.'
--- program.py
+++ program.py
@@ -8,63 +8,29 @@ Joe Nguyen | 01 Sep 2020
 import sys
 import importlib
 
-import hr
-import productivity
-import employees
-
+from hr import PayrollSystem, HourlyPolicy
+from productivity import ProductivitySystem
+from employees import EmployeeDatabase
 
 importlib.reload(sys.modules['hr'])
 importlib.reload(sys.modules['productivity'])
 importlib.reload(sys.modules['employees'])
 
+# ------------------------------------------------
+# policy-based design
+# ------------------------------------------------
+productivity_system = ProductivitySystem()
+payroll_system = PayrollSystem()
+employee_database = EmployeeDatabase()
 
-# c = fn.MyClass()
-# dir(c)
+# list comphrension to get employee attributes
+employees = employee_database.employees
 
-# o = object()
-# dir(o)
+# Manager changes to a temporary employee (hourly rate)
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
 
-# raise fn.MyError()
-
-# Payroll system
-salary_employee = employees.SalaryEmployee(1, 'John Smith', 1500)
-hourly_employee = employees.HourlyEmployee(2, 'Jane Doe', 40, 15)
-commission_employee = employees.CommissionEmployee(3, 'Kevin Bacon', 1000, 250)
-
-# Error case
-# employee = employees.Employee(1, 'Invalid')
-# ^TypeError: Can't instantiate abstract class Employee with abstract methods
-# calculate_payroll
-
-payroll_system = hr.PayrollSystem()
-employee_ls = [
-    salary_employee,
-    hourly_employee,
-    commission_employee,
-    # employee,
-]
-payroll_system.calculate_payroll(employee_ls)
-# ^AttributeError: 'Employee' object has no attribute 'calculate_payroll'
-
-# Payroll and productivty
-manager = employees.Manager(1, 'Mary Poppins', 3000)
-secretary = employees.Secretary(2, 'John Smith', 1500)
-sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
-factory_worker = employees.FactoryWorker(2, 'Jane Doe', 40, 15)
-temporary_secretary = employees.TemporarySecretary(5, 'Robin Williams', 40, 9)
-employee_ls = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-    temporary_secretary,
-]
-productivity_system = productivity.ProductivitySystem()
-productivity_system.track(employee_ls, 40)
-payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(employee_ls)
-
-
-# method resolution order
-# employees.TemporarySecretary.__mro__
-employees.Manager.__mro__
+# for loop to calculate .track()
+productivity_system.track(employees, 40)
+# for loop to calculate .calculate_payroll()
+payroll_system.calculate_payroll(employees)
